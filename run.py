ecfe57d975ebfd0afe107d4b09a86121c1486f8d
og2701 <67282414+og2701@users.noreply.github.com>
2024-06-13 18:50:11 +0100

    Update run.py

--- run.py
+++ run.py
@@ -7,23 +7,27 @@ app = create_app()
 
 async def shutdown(signal, frame):
     print("Shutting down...")
+    
     discord_bot.stop_bot()
+    
     socketio.stop()
-    
-    loop = asyncio.get_event_loop()
+
     tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
-    
     for task in tasks:
         task.cancel()
-    
+
     await asyncio.gather(*tasks, return_exceptions=True)
+    loop = asyncio.get_event_loop()
     loop.stop()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
 
-    signal.signal(signal.SIGINT, lambda s, f: asyncio.run(shutdown(s, f)))
-    signal.signal(signal.SIGTERM, lambda s, f: asyncio.run(shutdown(s, f)))
+    def shutdown_signal_handler(signal, frame):
+        asyncio.run(shutdown(signal, frame))
+    
+    signal.signal(signal.SIGINT, shutdown_signal_handler)
+    signal.signal(signal.SIGTERM, shutdown_signal_handler)
 
     discord_bot.start_bot(app)
     socketio.run(app, debug=True)
