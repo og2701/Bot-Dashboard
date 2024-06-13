c2752502e20b2a2ff4ffb0fa33124ebe8a854643
og2701 <67282414+og2701@users.noreply.github.com>
2024-06-13 18:37:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ python3 -m pip install -r requirements.txt
 while true
 do
     echo "Starting bot..."
-    python3 main.py
+    python3 run.py
     echo "Bot crashed with exit code $?. Restarting in 5 seconds..."
     sleep 5
 done
