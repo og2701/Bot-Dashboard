4c51bb1a3e808799b31dd15c521863b6f873a34b
og2701 <67282414+og2701@users.noreply.github.com>
2024-06-13 19:57:36 +0100

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, current_app
 
 auth = Blueprint('auth', __name__)
 
