58d5a6be447e732a02297640741eb1a29423c6f7
og2701 <67282414+og2701@users.noreply.github.com>
2024-06-13 20:26:11 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,34 +1,11 @@
 from app import create_app, socketio, discord_bot
-import signal
-import sys
 import asyncio
 
 app = create_app()
 
-async def shutdown():
-    print("Shutting down...")
-
-    discord_bot.stop_bot()
-
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is not None:
-        func()
-
-    tasks = [t for t in asyncio.all_tasks() if t is not asyncio.current_task()]
-    for task in tasks:
-        task.cancel()
-
-    await asyncio.gather(*tasks, return_exceptions=True)
-
-def shutdown_signal_handler(signal, frame):
-    loop = asyncio.get_event_loop()
-    loop.create_task(shutdown())
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-
-    signal.signal(signal.SIGINT, shutdown_signal_handler)
-    signal.signal(signal.SIGTERM, shutdown_signal_handler)
 
     discord_bot.start_bot(app)
     socketio.run(app, debug=False, allow_unsafe_werkzeug=True)
