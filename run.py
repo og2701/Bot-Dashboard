b0994892f93ba07564c08ce1faacb0ed4f1f0e06
og2701 <67282414+og2701@users.noreply.github.com>
2024-06-13 18:41:07 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-from app import create_app, socketio, discord_bot, diplomacy_bot
+from app import create_app, socketio, discord_bot
 
 app = create_app()
 
 if __name__ == "__main__":
     discord_bot.start_bot(app)
-    diplomacy_bot.start_bot(app)
     socketio.run(app, debug=True)
